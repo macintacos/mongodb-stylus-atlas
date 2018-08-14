308815cfa1a71bf4ea0b845f66ae5bf73e92a283
Julian Torres <julian.torres@mongodb.com>
2018-08-13 23:28:50 -0400

    Initial Phase complete. Now to theme the details.
    Signed-off-by: Julian Torres <julian.torres@mongodb.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
-"use strict";
+(function() {
+  "use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+  const gulp = require("gulp");
+  const sass = require("gulp-sass");
+  const sassLint = require("gulp-sass-lint");
 
-gulp.task('sass', function () {
-    return gulp.src('./src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./build'));
-});
+  gulp.task("sass", function() {
+    return gulp
+      .src("./src/**/*.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(gulp.dest("./build"));
+  });
 
-gulp.task('watch', function () {
-    gulp.watch('./src/**/*.scss', gulp.parallel('sass'));
-});
+  gulp.task("sass-lint", function() {
+    return gulp
+      .src("./src/**/*.s+(a|c)ss")
+      .pipe(
+        sassLint({
+          rules: {
+            "leading-zero": 0,
+            "nesting-depth": 0,
+            "no-important": 0,
+            "no-url-domains": 0,
+            "no-url-protocols": 0,
+            indentation: 0,
+            quotes: 0
+          }
+        })
+      )
+      .pipe(sassLint.format());
+  });
 
-gulp.task('default', gulp.series(['watch']));
+  gulp.task("watch", function() {
+    gulp.watch("./src/**/*.scss", gulp.parallel("sass", "sass-lint"));
+  });
+
+  gulp.task("default", gulp.series(["watch"]));
+})();
